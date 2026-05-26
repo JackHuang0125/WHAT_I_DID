9d8306481f801b8d5ce7aa36b7b0286b3983a347
Yang <boy850125@gmail.com>
2026-05-26 11:36:08 +0800

    整理

--- 練習/練習1_範例網址爬蟲.py
+++
@@ -1,7 +0,0 @@
-import requests as req
-from bs4 import BeautifulSoup as B
-url = 'https://www.iana.org/domains'
-resp = req.get(url)
-resp.encoding='utf-8'
-soup = B(resp.text ,'html.parser')
-print(soup)
